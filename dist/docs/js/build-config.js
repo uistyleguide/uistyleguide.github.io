5a0569aad7bbd05247be722109410750f1b4db2f
Eytan Anjel <eytan.anjel@citentcloud.com>
2015-03-26 12:17:15 -0700

    pushing other stuff

---
+++ dist/docs/js/build-config.js
@@ -0,0 +1,7 @@
+DocsApp.constant('BUILDCONFIG', {
+  "ngVersion": "1.3.2",
+  "version": "0.8.3",
+  "repository": "https://github.com/angular/material",
+  "commit": "5fe2e36674badf8f8cfa75306ef3056d47d0ea9f",
+  "date": "2015-03-26 13:28:43 -0500"
+});
