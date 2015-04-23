82bc2bd6c04903906ebc241b8bee868e719c4f50
uistyleguide <tgsteininger@gmail.com>
2015-04-23 13:26:32 -0700

    add message here

--- dist/docs/js/build-config.js
+++ dist/docs/js/build-config.js
@@ -2,6 +2,6 @@ DocsApp.constant('BUILDCONFIG', {
   "ngVersion": "1.3.2",
   "version": "0.8.3",
   "repository": "https://github.com/angular/material",
-  "commit": "5fe2e36674badf8f8cfa75306ef3056d47d0ea9f",
-  "date": "2015-03-26 13:28:43 -0500"
+  "commit": "05b0141a419cfc7d3da04e6809f54b3d8bca689c",
+  "date": "2015-04-22 11:55:20 -0700"
 });
