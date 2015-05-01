8ace4a43ed4723ae0d57af0fee1e81ca838fc08c
uistyleguide <tgsteininger@gmail.com>
2015-05-01 15:40:59 -0700

    Commiting so that I can try and merge dev to ad-material without errors

--- dist/docs/demo-partials/autocomplete/demoBasicUsage/script.js
+++ dist/docs/demo-partials/autocomplete/demoBasicUsage/script.js
@@ -4,16 +4,17 @@
       .module('autocompleteDemo', ['ngMaterial'])
       .controller('DemoCtrl', DemoCtrl);
 
-  function DemoCtrl ($timeout, $q) {
+  function DemoCtrl ($timeout, $q, $log) {
     var self = this;
+
+    self.simulateQuery = false;
+    self.isDisabled    = false;
 
     // list of `state` value/display objects
     self.states        = loadAll();
-    self.selectedItem  = null;
-    self.searchText    = null;
     self.querySearch   = querySearch;
-    self.simulateQuery = false;
-    self.isDisabled    = false;
+    self.selectedItemChange = selectedItemChange;
+    self.searchTextChange   = searchTextChange;
 
     // ******************************
     // Internal methods
@@ -24,7 +25,7 @@
      * remote dataservice call.
      */
     function querySearch (query) {
-      var results = query ? self.states.filter( createFilterFor(query) ) : [],
+      var results = query ? self.states.filter( createFilterFor(query) ) : self.states,
           deferred;
       if (self.simulateQuery) {
         deferred = $q.defer();
@@ -33,6 +34,14 @@
       } else {
         return results;
       }
+    }
+
+    function searchTextChange(text) {
+      $log.info('Text changed to ' + text);
+    }
+
+    function selectedItemChange(item) {
+      $log.info('Item changed to ' + JSON.stringify(item));
     }
 
     /**
